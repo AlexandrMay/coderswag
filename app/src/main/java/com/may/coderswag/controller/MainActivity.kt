a78b72c7df350fbcb9a8396c4e7b0321aaba7afa
AlexandrMay <maysalexandr@gmail.com>
2020-03-15 22:09:38 +0200

    homework

--- app/src/main/java/com/may/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/may/coderswag/controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.may.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.may.coderswag.R
+import com.may.coderswag.adapters.CategoryAdapter
+import com.may.coderswag.model.Category
+import com.may.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
